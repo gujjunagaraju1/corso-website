6a2cf07f93e1350574b3c152c6a846a3a3e254b3
gujjunagaraju1 <gujjunagaraju19@gmail.com>
2023-10-18 10:18:53 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function userScoll() {
+  const navbar = document.querySelector(".navbar");
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add("bg-dark");
+      navbar.classList.add("navbar-stricky");
+    } else {
+      navbar.classList.remove("bg-dark");
+      navbar.classList.remove("navbar-stricky");
+    }
+  });
+}
+document.addEventListener("DOMContentLoaded", userScoll);
